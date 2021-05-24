83f9b5e19392270794f92719f50e07951bd00e63
jakob <jakob.lamberthartmann.canada@gmail.com>
2021-05-24 02:28:30 +0200

    changes

--- src/split.c
+++ src/split.c
@@ -1,6 +1,7 @@
 #include"split.h"
 
 #include<stdlib.h>
+
 int compare_int(const void* left, const void* right){
 	int left_int = *((int *) left);
 	int right_int = *((int *) right);
@@ -11,6 +12,8 @@ int compare_int(const void* left, const void* right){
 		return 1;
 	return 0;
 }
+
+
 
 int* split_n(int *n, int width, int height){
 	int *res = malloc(sizeof(int) * (*n));
@@ -25,10 +28,13 @@ int* split_n(int *n, int width, int height){
 }
 
 
+
 int* split_30(int *n, int width, int height){
 	*n = 30;
 	return split_n(n, width, height);
 }
+
+
 
 int* split_whole_line(int *n, int width, int height){
 	int *res = malloc(sizeof(int));
